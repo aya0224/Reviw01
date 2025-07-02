89ea4e0dcb7da38e390b797764e2479e193c556b
aya0224 <a.tsuchiya.0224@gmail.com>
2025-07-02 19:25:58 +0900

    kadai

---
+++ src/Reviw01.java
@@ -0,0 +1,22 @@
+
+public class Reviw01 {
+
+    public static void main(String[] args) {
+        double zeinuki = 300.0;
+        double zei = 1.1;
+         
+        double result;
+        result = tax(zeinuki*zei); 
+        
+        System.out.println("300円の税込み価格は"+result+"円です");
+
+    
+               
+    }
+
+    private static double tax(double d) {
+        // TODO 自動生成されたメソッド・スタブ
+        return 330;
+    }
+  
+}
