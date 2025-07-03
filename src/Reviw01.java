c5344a2a3b67f5cd3c7c6eb4403d1e4de45866d0
aya0224 <a.tsuchiya.0224@gmail.com>
2025-07-03 22:41:24 +0900

    訂正

--- src/Reviw01.java
+++ src/Reviw01.java
@@ -2,14 +2,21 @@
 public class Reviw01 {
 
     public static void main(String[] args) {
-        double zeinuki = 300.0;
+        double zeinuki = 300;
         double zei = 0.1;
-         
+        
+        int i =(int)zeinuki;
+        
         double result;
         result  = tax(zeinuki,zei); 
         
+        int r = (int)result;
+            
+        
+                
+        
         String tax;
-        System.out.println(zeinuki+"の税込み価格は" + (zeinuki + result)+ "円" +("消費税は"+result +"円")+"です");
+        System.out.println(i+"の税込み価格は" + (i + r)+ "円" +("消費税は"+r +"円")+"です");
                
     }
 
