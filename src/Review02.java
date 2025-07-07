34148d240a86764adadebdd90fc37e43f62e0230
aya0224 <a.tsuchiya.0224@gmail.com>
2025-07-07 20:57:21 +0900

    課題

--- src/Review02.java
+++ src/Review02.java
@@ -2,7 +2,7 @@
 public class Review02 {
 
     public static void main(String[] args) {
-       for (int i = 1; i <100; i ++) {
+       for (int i = 1; i <101; i ++) {
            
           if(i % 3 ==0) {
           System.out.println("Fizz");
